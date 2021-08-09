ec6a89c29248944299d9243ac9d68180ad526ad3
baevres <88624125+baevres@users.noreply.github.com>
2021-08-09 15:09:44 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,10 @@
 import json
 
 def lambda_handler(event, context):
-    some_text = 'Hello Vasya!'
-    json_mylist = json.dumps(some_text, separators=(',', ':'))
+    some_text = 'Hello, World!'
+    json_response = json.dumps(some_text, separators=(',', ':'))
     
     return {
         'statusCode': 200,
-        'body': json_mylist
+        'body': json_response
     } 
