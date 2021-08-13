f2b9a0fe8c947c426cb121e0e9ef38fb83717796
baevres <88624125+baevres@users.noreply.github.com>
2021-08-13 20:11:59 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,9 @@
 import json
-
+import time
 import requests
 
 
-def circle(event=None, context=None):
+def circle():
     """
     Function take all records from MainTable - https://airtable.com/tbluOqbjxHEZoXg1r/viwKQMQcW0Baxtiim?blocks=hide
     with fields ID and title, sorted by ID.
@@ -13,51 +13,57 @@ def circle(event=None, context=None):
       ID - 5, world
     When iteration reach limit (Ex.: ID: 14), circle begins again
     """
-    import time
     key = 'key61gNeBIc9mp8RC' #my own api_key
     url = f'https://api.airtable.com/v0/appS74xV8aAeYBO5v/MainTable?fields%5B%5D=ID&fields%5B%5D=title&sort%5B0%5D%5Bfield%5D=ID&api_key={key}'
-
+    main_lst = []
     req = requests.get(url).json()['records']
     l = len(req) - 1 # for iteration
     i = 0
-    while i <= 10:
+    while i < 1:
         """
-        circle will be infinity, but every iteration have interval in 1 sec
+        The circle must had been infinity with iterations in 1 sec,
+        but now 'lambda_handler' must`ve to response with interval in 1 sec
         """
         for x in range(l + 1):
             if x <= l - 3:
+                mini_lst = []
                 for n in range(1, 4):
-                    print(f"ID - {req[x]['fields']['ID']},", req[x + n]['fields']['title'])
+                    mini_lst.append(f"ID - {req[x]['fields']['ID']}, {req[x + n]['fields']['title']}")
+                main_lst.append(mini_lst)
+                #time.sleep(1)
 
-                time.sleep(1)
-                print()
 
             elif (l - 3) < x < l:
+                mini_lst = []
                 rest = l - x
                 dif = 3 - rest
                 for n in range(1, rest+1):
-                    print(f"ID - {req[x]['fields']['ID']},", req[x + n]['fields']['title'])
+                    mini_lst.append(f"ID - {req[x]['fields']['ID']}, {req[x + n]['fields']['title']}")
 
                 for new_n in range(dif):
-                    print(f"ID - {req[x]['fields']['ID']},", req[new_n]['fields']['title'])
-                time.sleep(1)
-                print()
+                    mini_lst.append(f"ID - {req[x]['fields']['ID']}, {req[new_n]['fields']['title']}")
+                main_lst.append(mini_lst)
+
+                #time.sleep(1)
+
 
             elif x == l:
+                mini_lst = []
                 for n in range(3):
-                    print(f"ID - {req[x]['fields']['ID']},", req[n]['fields']['title'])
-                time.sleep(1)
-                print()
+                    mini_lst.append(f"ID - {req[x]['fields']['ID']}, {req[n]['fields']['title']}")
+                main_lst.append(mini_lst)
+                #time.sleep(1)
+
                 i += 1
+    return main_lst
 
 
-circle()
-
-def lambda_handler(event, context):
-    some_text = 'Hello, World!'
-    json_response = json.dumps(some_text, separators=(',', ':'))
-    
-    return {
-        'statusCode': 200,
-        'body': circle()
-    } 
+def lambda_handler(event=None, context=None):
+    text = circle()
+    for x in text:
+        print(x)
+        yield {
+            'status': 200,
+            'body': x
+        }
+        time.sleep(1)
